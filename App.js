eaa97cf2f7a771fdb01f1e6e94e28f5783a68ee7
Julliat Tom <tomjulliat07@gmail.com>
2026-03-02 13:47:22 +0100

    feat: Ajout goalContext a l'app

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import HomeScreen from './src/screens/HomeScreen';
+import { GoalsProvider } from './src/context/GoalsContext';
 
 export default function App() {
   return (
     <>
-      <HomeScreen />
-      <StatusBar style="light" />
+      <GoalsProvider>
+        <HomeScreen />
+        <StatusBar style="light" />
+      </GoalsProvider>
     </>
   );
 }
